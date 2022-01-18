f672bd6070181eaedb208e028de59bbb0069f139
Dima Poluda <derossi@list.ru>
2022-01-18 03:54:49 +0300

    projectStart

---
+++ script/main.js
@@ -0,0 +1,12 @@
+// $(function () {
+
+//     $('path').click(function () {
+//         $('.popup').addClass('is-open');
+//         return false;
+//     });
+    
+//     $('.popup_close').click(function () {
+//         $('.popup').removeClass('is-open');
+//     });
+
+// });
