64978a4910866db9bf1d437f2ec833e5964a33ab
Dima Poluda <derossi@list.ru>
2022-01-20 03:32:15 +0300

    fully finished station

--- ajax.php
+++ ajax.php
@@ -1,18 +1,20 @@
 <?php
 include "db/connect_db.php";
 // var_dump($_POST);
-if (isset($_POST['name'])) {
-    $val = $_POST['name'];
+if (isset($_POST['data'])) {
+    $stationValue = $_POST['data']['station'];
+    $lineValue = $_POST['data']['line'];
+    // echo $lineValue;
+    // print_r($_POST);
     // echo json_encode($val);//Необходимая строка
-    $goods = json_encode($val,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-    $query = "SELECT * FROM metroInfo WHERE NameOfStation = '$val' LIMIT 1";
-    // echo $query;
+    // $goods = json_encode($val,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+    $query = "SELECT * FROM metroInfo WHERE metroInfo.NameOfStation = '$stationValue' AND metroInfo.Line = '$lineValue' LIMIT 1";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($result);
     // print_r($row);
     // echo $row['NameOfStation'];
     $str = $row['District'];
-    echo json_encode($str);
+    echo json_encode($row);
 
 
 
